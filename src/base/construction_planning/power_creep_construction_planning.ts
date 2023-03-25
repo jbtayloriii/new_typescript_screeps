e68255d9419971725a9a6e6c3e85e8decdf233c5
Josh Taylor <joshbtayloriii@gmail.com>
2023-03-25 11:02:15 -0500

    Remove power harvesting memory and create base only based on room

--- src/base/construction_planning/power_creep_construction_planning.ts
+++ src/base/construction_planning/power_creep_construction_planning.ts
@@ -1,34 +1,45 @@
 import { ConstructionPlanning } from "../construction_planning";
 
-
 export class PowerCreepConstructionPlanning implements ConstructionPlanning {
-  constructor() {
-
-  }
+  constructor() {}
 
   plan(room: Room): void {
-    const mem = room.memory;
-    if (!mem.powerHarvestingMap) {
-      mem.powerHarvestingMap = {};
-    }
-
     const spawns = room.find(FIND_MY_SPAWNS);
     if (spawns.length == 0) {
       return;
     }
 
     for (let source of room.find(FIND_SOURCES) as Array<Source>) {
-      if (!(source.id in mem.powerHarvestingMap)) {
-        const containerPos = this.createPowerHarvestingPath(room, spawns[0], source);
+      const nearbyContainers = source.pos.findInRange(
+        FIND_MY_STRUCTURES,
+        /* range= */ 1,
+        {
+          filter: { structureType: STRUCTURE_CONTAINER },
+        }
+      );
+      const nearbyContainerSites = source.pos.findInRange(
+        FIND_MY_CONSTRUCTION_SITES,
+        /* range= */ 1,
+        {
+          filter: { structureType: STRUCTURE_CONTAINER },
+        }
+      );
 
-        mem.powerHarvestingMap[source.id] = containerPos;
+      if (nearbyContainers.length == 0 && nearbyContainerSites.length == 0) {
+        this.createPowerHarvestingPath(room, spawns[0], source);
+
         return;
       }
     }
   }
 
   private createPowerHarvestingPath(room: Room, start: Structure, end: Source) {
-    const pathArr = PathFinder.search(start.pos, {pos: end.pos, range: 1}).path;
+    const pathArr = PathFinder.search(start.pos, {
+      pos: end.pos,
+      range: 1,
+    }).path;
+
+    pathArr[pathArr.length - 1].createConstructionSite(STRUCTURE_CONTAINER);
     for (let pathPos of pathArr) {
       if (pathPos.isEqualTo(start.pos) || pathPos.isEqualTo(end.pos)) {
         continue;
@@ -40,18 +51,21 @@ export class PowerCreepConstructionPlanning implements ConstructionPlanning {
       }
     }
 
-    // Add a container to the end of the path
-    pathArr[pathArr.length - 1].createConstructionSite(STRUCTURE_CONTAINER);
-
     return pathArr[pathArr.length - 1];
   }
 
   private shouldAddRoad(pos: RoomPosition, source: Source): boolean {
     for (let lookObj of pos.look()) {
-      if (lookObj.type == LOOK_CONSTRUCTION_SITES && lookObj[LOOK_CONSTRUCTION_SITES]?.structureType == STRUCTURE_ROAD) {
+      if (
+        lookObj.type == LOOK_CONSTRUCTION_SITES &&
+        lookObj[LOOK_CONSTRUCTION_SITES]?.structureType == STRUCTURE_ROAD
+      ) {
         return false;
       }
-      if (lookObj.type == LOOK_STRUCTURES && lookObj[LOOK_STRUCTURES]?.structureType == STRUCTURE_ROAD ) {
+      if (
+        lookObj.type == LOOK_STRUCTURES &&
+        lookObj[LOOK_STRUCTURES]?.structureType == STRUCTURE_ROAD
+      ) {
         return false;
       }
     }
