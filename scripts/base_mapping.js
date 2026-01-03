4472466e0e7b00406461b3ffcf3609c7b81a5018
Josh Taylor <joshbtayloriii@gmail.com>
2026-01-02 20:37:33 -0600

    Create script for pulling room data

--- scripts/base_mapping.js
+++ scripts/base_mapping.js
@@ -1,22 +1,45 @@
 import * as path from 'path';
 import fs from 'node:fs';
-import { json } from 'node:stream/consumers';
 
 console.log("Testing");
 
 const auth_codes_filepath = path.join(process.cwd(), "scripts", "auth_codes.json");
 
 const URL_ENDPOINT = "https://screeps.com/api/";
-const TERRAIN_URL = path.join(URL_ENDPOINT, "/api/game/room-terrain");
+const TERRAIN_URL = path.join(URL_ENDPOINT, "/game/room-terrain");
 
 
-fs.readFile(auth_codes_filepath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+function getRoomData(roomName) {
+  fs.readFile(auth_codes_filepath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-  let authCode = JSON.parse(data).code;
+    let authCode = JSON.parse(data).code;
 
-  console.log(authCode);
-});
+    let url = new URL(TERRAIN_URL);
+    url.searchParams.set("_token", authCode);
+    url.searchParams.set("shard", "shard0");
+    url.searchParams.set("room", roomName);
+
+    console.log(url.toString());
+
+    fetch(url.toString())
+      .then(response => response.json())
+      .then(data => writeRoomData(roomName, data))
+      .catch(error => console.error('Error:', error));
+  });
+}
+
+function writeRoomData(roomName, roomData) {
+  let roomFilepath = path.join(process.cwd(), "data", "rooms", roomName + ".json");
+  fs.writeFile(roomFilepath, JSON.stringify(roomData, null, 2), err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+
+getRoomData("E23S15");
