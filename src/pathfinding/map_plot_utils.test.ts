a13eb84c16b6871801cbbbc733ef3a01e75a4a7d
Josh Taylor <joshbtayloriii@gmail.com>
2026-01-04 22:29:24 -0600

    Refactor diamond mapping and add a square mapping function.

---
+++ src/pathfinding/map_plot_utils.test.ts
@@ -0,0 +1,231 @@
+import { describe, expect, test } from '@jest/globals';
+import { getDiamondMapping, getSquareMapping, initializeEdges, WeightedPosition } from './map_plot_utils';
+
+describe('Initialization', () => {
+    test('Initialize edges 5x5', () => {
+        let mapSize = 5;
+        let tileArr = Array.from({ length: mapSize }, () => Array(mapSize).fill(0));
+        let visitedArr = Array.from({ length: mapSize }, () => Array(mapSize).fill(false));
+        let queue: WeightedPosition[] = [];
+
+        let expectedTileArr = [
+            [-1, -1, -1, -1, -1],
+            [-1, 0, 0, 0, -1],
+            [-1, 0, 0, 0, -1],
+            [-1, 0, 0, 0, -1],
+            [-1, -1, -1, -1, -1],
+        ]
+
+        let expectedVisitedArr = [
+            [true, true, true, true, true],
+            [true, true, true, true, true],
+            [true, true, false, true, true],
+            [true, true, true, true, true],
+            [true, true, true, true, true],
+        ]
+
+        initializeEdges(tileArr, visitedArr, queue, mapSize);
+
+        // One element for each cell directly inside
+        expect(queue.length).toBe(8);
+        expect(tileArr).toStrictEqual(expectedTileArr);
+        expect(visitedArr).toStrictEqual(expectedVisitedArr);
+    });
+
+    test('Initialize edges 10x10', () => {
+        let mapSize = 10;
+        let tileArr = Array.from({ length: mapSize }, () => Array(mapSize).fill(0));
+        let visitedArr = Array.from({ length: mapSize }, () => Array(mapSize).fill(false));
+        let queue: WeightedPosition[] = [];
+
+        let expectedVisitedArr = [
+            [true, true, true, true, true, true, true, true, true, true],
+            [true, true, true, true, true, true, true, true, true, true],
+            [true, true, false, false, false, false, false, false, true, true],
+            [true, true, false, false, false, false, false, false, true, true],
+            [true, true, false, false, false, false, false, false, true, true],
+            [true, true, false, false, false, false, false, false, true, true],
+            [true, true, false, false, false, false, false, false, true, true],
+            [true, true, false, false, false, false, false, false, true, true],
+            [true, true, true, true, true, true, true, true, true, true],
+            [true, true, true, true, true, true, true, true, true, true]
+        ]
+
+        let expectedTileArr = [
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        ]
+
+        initializeEdges(tileArr, visitedArr, queue, mapSize);
+
+        // One element for each cell directly inside -> 8, 8, 6, 6 for walls
+        expect(queue.length).toBe(28);
+        expect(tileArr).toStrictEqual(expectedTileArr);
+        expect(visitedArr).toStrictEqual(expectedVisitedArr);
+    });
+});
+
+describe('Diamond mapping', () => {
+    test('No walls 10x10', () => {
+        let actual = getDiamondMapping([], 10);
+
+        let expected = [
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 2, 3, 3, 3, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 4, 4, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 4, 4, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 3, 3, 3, 2, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+
+    test('Inner walls 10x10', () => {
+        let walls = [
+            { x: 1, y: 1 },
+            { x: 2, y: 1 },
+            { x: 5, y: 5 },
+        ];
+        let actual = getDiamondMapping(walls, 10);
+
+        let expected = [
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, 1, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 2, 3, 3, 2, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 2, 1, 2, 2, 1, -1],
+            [-1, 1, 2, 2, 1, -1, 1, 2, 1, -1],
+            [-1, 1, 2, 3, 2, 1, 2, 2, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+
+    test('No walls 5x5', () => {
+        let actual = getDiamondMapping([], 5);
+
+        let expected = [
+            [-1, -1, -1, -1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, 1, 2, 1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+
+    test('Inner walls 5x5', () => {
+        let walls = [{ x: 1, y: 1 }, { x: 2, y: 1 }];
+        let actual = getDiamondMapping(walls, 5);
+
+        let expected = [
+            [-1, -1, -1, -1, -1],
+            [-1, -1, -1, 1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+});
+
+
+describe('Square mapping', () => {
+    test('No walls 10x10', () => {
+        let actual = getSquareMapping([], 10);
+
+        let expected = [
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 2, 3, 3, 3, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 4, 4, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 4, 4, 3, 2, 1, -1],
+            [-1, 1, 2, 3, 3, 3, 3, 2, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+
+    test('Inner walls 10x10', () => {
+        let walls = [
+            { x: 1, y: 1 },
+            { x: 2, y: 1 },
+            { x: 5, y: 5 },
+        ];
+        let actual = getSquareMapping(walls, 10);
+
+        let expected = [
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, 1, 1, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 2, 2, 1, 1, 1, 2, 1, -1],
+            [-1, 1, 2, 2, 1, -1, 1, 2, 1, -1],
+            [-1, 1, 2, 2, 1, 1, 1, 2, 1, -1],
+            [-1, 1, 2, 2, 2, 2, 2, 2, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+
+    test('No walls 5x5', () => {
+        let actual = getSquareMapping([], 5);
+
+        let expected = [
+            [-1, -1, -1, -1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, 1, 2, 1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+
+    test('Inner walls 5x5', () => {
+        let walls = [{ x: 1, y: 1 }, { x: 2, y: 1 }];
+        let actual = getSquareMapping(walls, 5);
+
+        let expected = [
+            [-1, -1, -1, -1, -1],
+            [-1, -1, -1, 1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, 1, 1, 1, -1],
+            [-1, -1, -1, -1, -1],
+        ]
+
+        expect(mappingToPrintable(actual)).toStrictEqual(mappingToPrintable(expected));
+    });
+});
+
+/** Converts mappings to a more printable version, for test output printing.
+ * 
+ * Each mapping row is converted to a string with 3 characters used for each value.
+ */
+function mappingToPrintable(mapping: number[][]): string[] {
+    return mapping.map(row => row.map(v => `${v}`.padStart(3, " ")).join(""));
+}
---
+++ src/pathfinding/map_plot_utils.ts
@@ -0,0 +1,175 @@
+/**
+ * Module for performing mapping calculations for base planning.
+ */
+
+type Position = {
+    x: number,
+    y: number,
+}
+
+export type WeightedPosition = {
+    x: number,
+    y: number,
+    v: number
+}
+
+const DEFAULT_MAP_SIZE = 50;
+
+/** Returns a 2d array giving the orthogonal AND diagonal distance to walls or edges.
+ * 
+ *  The array is returned as map[y][x], top to bottom, left to right.
+ * 
+ *  This is used for making square stamps of radius R: 
+ * 
+ *  W11111W    WSSSSSW
+ *  1122211    .SSSSS.
+ *  1223221 -> .SSXSS.
+ *  1122211    .SSSSS.
+ *  W11111W    WSSSSSW
+ */
+export function getSquareMapping(walls: Position[], mapSize: number = DEFAULT_MAP_SIZE): number[][] {
+    let queueFn = function (queue: WeightedPosition[], visitedArr: boolean[][], nextVal: WeightedPosition) {
+        for (let y = nextVal.y - 1; y <= nextVal.y + 1; y++) {
+            for (let x = nextVal.x - 1; x <= nextVal.x + 1; x++) {
+                if (y == nextVal.y && x == nextVal.x) {
+                    continue;
+                }
+                if (y < 0 || x < 0) {
+                    continue;
+                }
+                if (y >= mapSize || x >= mapSize) {
+                    continue;
+                }
+                if (visitedArr[y][x]) {
+                    continue;
+                }
+
+                queue.push({ x: x, y: y, v: nextVal.v + 1 });
+            }
+        }
+    }
+
+    return performMapping(walls, mapSize, queueFn);
+}
+
+/** Returns a 2d array giving the orthogonal distance to walls or edges.
+ * 
+ *  The array is returned as map[y][x], top to bottom, left to right.
+ * 
+ *  This is used for making diamond stamps of radius R: 
+ * 
+ *  W12321W    W..D..W
+ *  1234321    ..DDD..
+ *  2345432 -> .DDXDD.
+ *  1234321    ..DDD..
+ *  W12321W    W..D..W
+ */
+export function getDiamondMapping(walls: Position[], mapSize: number = DEFAULT_MAP_SIZE): number[][] {
+    let queueFn = function (queue: WeightedPosition[], visitedArr: boolean[][], nextVal: WeightedPosition) {
+        let lessX = nextVal.x - 1;
+        let moreX = nextVal.x + 1;
+        let lessY = nextVal.y - 1;
+        let moreY = nextVal.y + 1;
+
+        if (!visitedArr[nextVal.y][lessX] && lessX >= 0) {
+            visitedArr[nextVal.y][lessX] = true;
+            queue.push({ x: lessX, y: nextVal.y, v: nextVal.v + 1 });
+        }
+        if (!visitedArr[nextVal.y][moreX] && moreX < mapSize) {
+            visitedArr[nextVal.y][moreX] = true;
+            queue.push({ x: moreX, y: nextVal.y, v: nextVal.v + 1 });
+        }
+
+        if (!visitedArr[lessY][nextVal.x] && lessY >= 0) {
+            visitedArr[lessY][nextVal.x] = true;
+            queue.push({ x: nextVal.x, y: lessY, v: nextVal.v + 1 });
+        }
+        if (!visitedArr[moreY][nextVal.x] && moreY < mapSize) {
+            visitedArr[moreY][nextVal.x] = true;
+            queue.push({ x: nextVal.x, y: moreY, v: nextVal.v + 1 });
+        }
+    }
+
+    return performMapping(walls, mapSize, queueFn);
+}
+
+
+/** Internal function that performs diamond and/or square mapping, based on queue additions. */
+function performMapping(walls: Position[], mapSize: number, queueFn: (queue: WeightedPosition[], visitedArr: boolean[][], val: WeightedPosition) => void) {
+    let tileArr: number[][] = Array.from({ length: mapSize }, () => Array(mapSize).fill(0));
+    let visitedArr: boolean[][] = Array.from({ length: mapSize }, () => Array(mapSize).fill(false));
+    let queue: WeightedPosition[] = [];
+
+    initializeEdges(tileArr, visitedArr, queue, mapSize);
+
+    // Add walls and push values next to walls
+    walls.forEach((row) => {
+        tileArr[row.y][row.x] = -1;
+        visitedArr[row.y][row.x] = true;
+        queueFn(queue, visitedArr, { x: row.x, y: row.y, v: 0 });
+    });
+
+    // BFS
+    while (queue.length > 0) {
+        let nextVal = queue.shift()!;
+
+        if (nextVal.x >= mapSize || nextVal.x < 0) {
+            continue;
+        }
+
+        if (nextVal.y >= mapSize || nextVal.y < 0) {
+            continue;
+        }
+        if (tileArr[nextVal.y][nextVal.x] != 0) {
+            continue;
+        }
+
+        tileArr[nextVal.y][nextVal.x] = nextVal.v;
+        visitedArr[nextVal.y][nextVal.x] = true;
+        queueFn(queue, visitedArr, nextVal);
+    }
+
+    return tileArr;
+}
+
+/** Initializes values in the return array and intenral queue. */
+export function initializeEdges(
+    tileArr: number[][],
+    visitedArr: boolean[][],
+    queue: WeightedPosition[],
+    mapSize: number
+): void {
+    // Add top/bottom edges
+    for (let i = 0; i < mapSize; i++) {
+        tileArr[0][i] = -1;
+        tileArr[mapSize - 1][i] = -1
+
+        visitedArr[0][i] = true;
+        visitedArr[mapSize - 1][i] = true;
+
+        if (i > 0 && i < mapSize - 1) {
+            queue.push({ x: i, y: 1, v: 1 });
+            queue.push({ x: i, y: mapSize - 2, v: 1 });
+
+            visitedArr[1][i] = true;
+            visitedArr[mapSize - 2][i] = true;
+        }
+    }
+
+    // Add left/right edges
+    for (let i = 1; i < mapSize - 1; i++) {
+        tileArr[i][0] = -1;
+        tileArr[i][mapSize - 1] = -1
+
+        visitedArr[i][0] = true;
+        visitedArr[i][mapSize - 1] = true;
+
+        if (i > 1 && i < mapSize - 2) {
+            queue.push({ x: 1, y: i, v: 1 });
+            queue.push({ x: mapSize - 2, y: i, v: 1 });
+
+            visitedArr[i][1] = true;
+            visitedArr[i][mapSize - 2] = true;
+        }
+    }
+}
