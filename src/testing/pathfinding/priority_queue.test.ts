1c1d594e7c9749c966f83fa33aa948f1e5291edd
Josh Taylor <joshbtayloriii@gmail.com>
2026-01-22 23:40:58 -0600

    Add a priority queue for pathfinding.

---
+++ src/testing/pathfinding/priority_queue.test.ts
@@ -0,0 +1,117 @@
+import { describe, expect, test } from '@jest/globals';
+import { PriorityQueue } from './priority_queue';
+
+describe('Basic checks', () => {
+    test('isEmpty() is true in empty queue', () => {
+        const queue = new PriorityQueue();
+        expect(queue.isEmpty()).toBe(true);
+    });
+
+    test('pop() is undefined in empty queue', () => {
+        const queue = new PriorityQueue();
+        expect(queue.pop()).toBe(undefined);
+    });
+
+    test('Repeated getPriority() calls are same', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 67, /* priority= */ 400);
+
+        expect(queue.getPriority(/* index= */ 67)).toBe(400);
+        expect(queue.getPriority(/* index= */ 67)).toBe(400);
+        expect(queue.getPriority(/* index= */ 67)).toBe(400);
+
+        expect(queue.getPriority(/* index= */ 68)).toBe(undefined);
+        expect(queue.getPriority(/* index= */ 68)).toBe(undefined);
+        expect(queue.getPriority(/* index= */ 68)).toBe(undefined);
+    });
+});
+
+describe('Inserting', () => {
+    test('Inserting 3 items pops items in priority order', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 1, /* priority= */ 10);
+        queue.insert(/* index= */ 2, /* priority= */ 50);
+        queue.insert(/* index= */ 3, /* priority= */ 30);
+
+        expect(queue.pop()).toStrictEqual({ index: 1, priority: 10 });
+        expect(queue.pop()).toStrictEqual({ index: 3, priority: 30 });
+        expect(queue.pop()).toStrictEqual({ index: 2, priority: 50 });
+
+        expect(queue.isEmpty()).toBe(true);
+    });
+
+    test('isEmpty() is false after insert', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 3, /* priority= */ 30);
+
+        expect(queue.isEmpty()).toBe(false);
+    });
+
+    test('Inserting and clear returns empty queue', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 1, /* priority= */ 10);
+        queue.insert(/* index= */ 2, /* priority= */ 50);
+
+        expect(queue.isEmpty()).toBe(false);
+        expect(queue.getPriority(/* index= */ 1)).toBe(10);
+        expect(queue.getPriority(/* index= */ 2)).toBe(50);
+
+        queue.clear();
+
+        expect(queue.isEmpty()).toBe(true);
+        expect(queue.getPriority(/* index= */ 1)).toBe(undefined);
+        expect(queue.getPriority(/* index= */ 2)).toBe(undefined);
+    });
+});
+
+describe('Changing priority', () => {
+    test('Updating to higher priority does not affect nodes', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 1, /* priority= */ 10);
+        queue.insert(/* index= */ 2, /* priority= */ 50);
+        queue.insert(/* index= */ 3, /* priority= */ 30);
+
+        // Node priority is updated to higher values, should not see change in queue
+        queue.update(/* index= */ 2, /* priority= */ 75);
+        queue.update(/* index= */ 1, /* priority= */ 15);
+
+        expect(queue.pop()).toStrictEqual({ index: 1, priority: 10 });
+        expect(queue.pop()).toStrictEqual({ index: 3, priority: 30 });
+        expect(queue.pop()).toStrictEqual({ index: 2, priority: 50 });
+
+        expect(queue.isEmpty()).toBe(true);
+    });
+
+    test('Updating priority with changing priority order affects pop() order', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 99, /* priority= */ 10);
+        queue.insert(/* index= */ 55, /* priority= */ 50);
+        queue.insert(/* index= */ 77, /* priority= */ 30);
+
+        // Node priority is updated to lower values, and pop() order changes
+        queue.update(/* index= */ 55, /* priority= */ 20);
+
+        expect(queue.pop()).toStrictEqual({ index: 99, priority: 10 });
+        expect(queue.pop()).toStrictEqual({ index: 55, priority: 20 });
+        expect(queue.pop()).toStrictEqual({ index: 77, priority: 30 });
+
+        expect(queue.isEmpty()).toBe(true);
+    });
+
+    test('Updating priority with not changing priority order does not affect pop() order', () => {
+        const queue = new PriorityQueue();
+        queue.insert(/* index= */ 321, /* priority= */ 100);
+        queue.insert(/* index= */ 432, /* priority= */ 500);
+        queue.insert(/* index= */ 543, /* priority= */ 300);
+
+        // Node priorities are updated to lower values, but pop order stays same
+        queue.update(/* index= */ 432, /* priority= */ 450);
+        queue.update(/* index= */ 543, /* priority= */ 150);
+
+        expect(queue.pop()).toStrictEqual({ index: 321, priority: 100 });
+        expect(queue.pop()).toStrictEqual({ index: 543, priority: 150 });
+        expect(queue.pop()).toStrictEqual({ index: 432, priority: 450 });
+
+        expect(queue.isEmpty()).toBe(true);
+    });
+});
---
+++ src/testing/pathfinding/priority_queue.ts
@@ -0,0 +1,122 @@
+
+
+export class PriorityQueue {
+    private priorities: Map<number, number>;
+    private heap: number[];
+    private size: number;
+
+    constructor() {
+        this.priorities = new Map();
+        this.heap = [];
+        this.size = 0;
+    }
+
+    /** Returns true iff there are no elements in the priority queue. */
+    isEmpty(): boolean {
+        return this.size === 0;
+    }
+
+    /** Returns the priority for a given index, or undefined if the index is not in the queue. */
+    getPriority(index: number): number | undefined {
+        return this.priorities.get(index);
+    }
+
+    /** Removes the top element from the queue and returns it and its priority. */
+    pop(): { index: number, priority: number | undefined } | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        // Get top value from the heap before bubbling
+        const retIndex = this.heap[1];
+        const retPriority = this.priorities.get(retIndex);
+        const ret = {
+            index: retIndex,
+            priority: retPriority,
+        }
+
+        this.heap[1] = this.heap[this.size];
+        this.size -= 1;
+
+        //
+        let vv: number = 1;
+        do {
+            const uu: number = vv;
+
+            if ((uu << 1) + 1 <= this.size) {
+                // Left and right children are in heap
+
+                // Compare with left child, swap if left child priority is lower
+                if (this.priorities.get(this.heap[uu])! >= this.priorities.get(this.heap[uu << 1])!) {
+                    vv = uu << 1;
+                }
+
+                // Compare with right child, swap if right child priority is lower
+                if (this.priorities.get(this.heap[vv])! >= this.priorities.get(this.heap[(uu << 1) + 1])!) {
+                    vv = (uu << 1) + 1;
+                }
+            } else if (uu << 1 <= this.size) {
+                // Only left child is available
+
+                // Compare with left child, swap if left child priority is lower
+                if (this.priorities.get(this.heap[uu])! >= this.priorities.get(this.heap[uu << 1])!) {
+                    vv = uu << 1;
+                }
+            }
+
+            if (uu === vv) {
+                break;
+            } else {
+                const temp = this.heap[uu];
+                this.heap[uu] = this.heap[vv];
+                this.heap[vv] = temp;
+            }
+
+        } while (true);
+
+        return ret;
+    }
+
+    insert(index: number, priority: number): void {
+        this.priorities.set(index, priority);
+        this.size += 1;
+        this.heap[this.size] = index;
+        this.bubbleUp(this.size);
+    }
+
+    update(index: number, priority: number): void {
+        for (let ii = this.size; ii > 0; ii--) {
+            if (this.heap[ii] == index) {
+                // Only update if the new priority is lower
+                if (this.priorities.get(index)! <= priority) {
+                    return;
+                }
+
+                this.priorities.set(index, priority);
+                this.bubbleUp(ii);
+                return;
+            }
+        }
+    }
+
+    clear(): void {
+        this.priorities = new Map();
+        this.heap = [];
+        this.size = 0;
+    }
+
+    /** Bubbles a newly inserted or updated node to a valid position in the heap. */
+    private bubbleUp(ii: number): void {
+        while (ii !== 1) {
+            if (this.priorities.get(this.heap[ii])! <= this.priorities.get(this.heap[ii >> 1])!) {
+                // Swap node with its parent, it is lower priority
+                const temp = this.heap[ii];
+                this.heap[ii] = this.heap[ii >> 1];
+                this.heap[ii >> 1] = temp;
+                ii = ii >> 1;
+            } else {
+                return;
+            }
+        }
+    }
+}
